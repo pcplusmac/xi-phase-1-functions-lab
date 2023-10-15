8f621316382468b3911979109746136d0e6fddfe
el-sam <samurai.h@yahoo.com>
2023-10-15 17:14:18 +0800

    pass first and 2nd test.

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Code your solution in this file!
+const hq = 42;
+// declare 'pickUp' variable to keep the value of pick-up block
+let pickUp; 
+// define function distanceFromHqInBlocks
+function distanceFromHqInBlocks(pickUp) {
+    let blocks = pickUp - hq;
+    return blocks;
+
+}
